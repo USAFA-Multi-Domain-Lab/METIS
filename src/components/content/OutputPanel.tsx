27932e4c665a612e5854c86d6719d12f231c8ea3
jsthomas <jsthomas1288@gmail.com>
2022-09-02 16:52:08 -0500

    Mission map box and output box created on dashboard page

---
+++ src/components/content/OutputPanel.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './OutputPanel.scss'
+
+const OutputBox = () => {
+  return (
+    <div className='OutputBox'>
+      <div className='BorderBox'>
+        <div className='Text'>Text</div>
+      </div>
+    </div>
+  )
+}
+
+export default OutputBox
