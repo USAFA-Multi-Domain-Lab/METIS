65ae3f2d89070271f8166166e1951105f193c168
Jordan M. Stokes <jordan.stokes@salientcrgt.com>
2023-01-26 14:26:21 -0600

    Added validation for the nodeStructure.

--- database/models/model-mission.ts
+++ database/models/model-mission.ts
@@ -19,6 +19,126 @@ const isNonNegativeInteger = (value: number): boolean => {
 
 /* -- SCHEMA VALIDATORS -- */
 
+// Validator for missions.
+const validate_missions = (mission: any, next: any): void => {
+  let parentNodeStructure: any = mission.nodeStructure
+  let nodeData: Array<any> = mission.nodeData
+  let nodeDataIDs: Array<string> = nodeData.map((node) => node.nodeID)
+  let correspondingNodeStructureIDs: Array<string> = []
+  let results: { error?: Error } = {}
+
+  // This will ensure that all nodes
+  // have IDs unique to the mission,
+  // and that all action IDs are unique
+  // to the node they are assigned to.
+  const ensureUniqueIDs = (): { error?: Error } => {
+    let nodeIDs: Array<string> = []
+
+    for (let nodeDatum of mission.nodeData) {
+      if (!nodeIDs.includes(nodeDatum.nodeID)) {
+        nodeIDs.push(nodeDatum.nodeID)
+
+        let actionIDs: Array<string> = []
+
+        for (let action of nodeDatum.actions) {
+          if (!actionIDs.includes(action.actionID)) {
+            actionIDs.push(action.actionID)
+          } else {
+            let error: Error = new Error(
+              `Error in nodeData:\nDuplicate actionID (${action.actionID}) used in node (${nodeDatum.nodeID}).`,
+            )
+            error.name = ERROR_BAD_DATA
+            return { error }
+          }
+        }
+      } else {
+        let error: Error = new Error(
+          `Error in nodeData:\nDuplicate nodeID used (${nodeDatum.nodeID}).`,
+        )
+        error.name = ERROR_BAD_DATA
+        return { error }
+      }
+    }
+    return {}
+  }
+
+  // This will ensure the node structure
+  // is valid.
+  const validateNodeStructure = (
+    nodeStructure: any = parentNodeStructure,
+    parentKey: string = 'ROOT',
+  ): { error?: Error } => {
+    if (!(nodeStructure instanceof Object)) {
+      let error: Error = new Error(
+        `Error in nodeStructure:\n"${parentKey}" is set to ${nodeStructure}, which is not an object.`,
+      )
+      error.name = ERROR_BAD_DATA
+      return { error }
+    }
+
+    for (let [key, value] of Object.entries(nodeStructure)) {
+      if (!nodeDataIDs.includes(key)) {
+        let error: Error = new Error(
+          `Error in mission:\n"${key}" was referenced in nodeStructure, but it was not found in the nodeData.`,
+        )
+        error.name = ERROR_BAD_DATA
+        return { error }
+      }
+      if (correspondingNodeStructureIDs.includes(key)) {
+        let error: Error = new Error(
+          `Error in nodeStructure:\nDuplicate nodeID used (${key}).`,
+        )
+        error.name = ERROR_BAD_DATA
+        return { error }
+      } else {
+        correspondingNodeStructureIDs.push(key)
+      }
+
+      let results: { error?: Error } = validateNodeStructure(value, key)
+
+      if (results.error) {
+        return results
+      }
+    }
+
+    return {}
+  }
+
+  // Ensure unique IDs
+  results = ensureUniqueIDs()
+
+  // Check for error.
+  if (results.error) {
+    return next(results.error)
+  }
+
+  // Validate node structure.
+  results = validateNodeStructure()
+
+  // Check for error.
+  if (results.error) {
+    return next(results.error)
+  }
+
+  // If the number of IDs in the nodeData
+  // is different than the number in the
+  // node structure, than the bad ID is
+  // found and an error is created for it.
+  if (nodeDataIDs.length !== correspondingNodeStructureIDs.length) {
+    for (let nodeID of nodeDataIDs) {
+      if (!correspondingNodeStructureIDs.includes(nodeID)) {
+        let error: Error = new Error(
+          `Error in mission:\n"${nodeID}" was referenced in nodeData, but it was not found in the nodeStructure.`,
+        )
+        error.name = ERROR_BAD_DATA
+        return next(error)
+      }
+    }
+  }
+
+  return next()
+}
+
 // Validator for missions.initialResources.
 const validate_missions_initialResources = (
   initialResources: number,
@@ -114,8 +234,10 @@ export const MissionSchema: Schema = new Schema(
       validate: validate_missions_initialResources,
     },
     deleted: { type: Boolean, required: true, default: false },
-    // ! ADD VALIDATOR
-    nodeStructure: { type: {}, required: true },
+    nodeStructure: {
+      type: {},
+      required: true,
+    },
     nodeData: {
       type: [
         {
@@ -186,38 +308,6 @@ export const MissionSchema: Schema = new Schema(
 
 /* -- FILTERS -- */
 
-const enforceUniqueIDs = (mission: any, next: any): void => {
-  let nodeIDs: Array<string> = []
-
-  for (let nodeDatum of mission.nodeData) {
-    if (!nodeIDs.includes(nodeDatum.nodeID)) {
-      nodeIDs.push(nodeDatum.nodeID)
-
-      let actionIDs: Array<string> = []
-
-      for (let action of nodeDatum.actions) {
-        if (!actionIDs.includes(action.actionID)) {
-          actionIDs.push(action.actionID)
-        } else {
-          let error: Error = new Error(
-            `Duplicate actionID (${action.actionID}) used in node (${nodeDatum.nodeID}).`,
-          )
-          error.name = ERROR_BAD_DATA
-          next(error)
-        }
-      }
-    } else {
-      let error: Error = new Error(
-        `Duplicate nodeID used in mission: ${nodeDatum.nodeID}`,
-      )
-      error.name = ERROR_BAD_DATA
-      next(error)
-    }
-  }
-
-  next()
-}
-
 const filterOutUnusedIDs = (missions: any): void => {
   let mission: any
 
@@ -248,10 +338,10 @@ const filterOutUnusedIDs = (missions: any): void => {
 }
 
 MissionSchema.pre('save', function (next) {
-  enforceUniqueIDs(this, next)
+  validate_missions(this, next)
 })
 MissionSchema.pre('update', function (next) {
-  enforceUniqueIDs(this, next)
+  validate_missions(this, next)
 })
 MissionSchema.post(/^find/, function (docs) {
   filterOutUnusedIDs(docs)
