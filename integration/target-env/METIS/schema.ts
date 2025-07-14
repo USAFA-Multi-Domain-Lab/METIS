100a791f56e8932231f601ba63a10a8356f1c499
Jordan Stokes <jordan.stokes@govcio.com>
2025-07-14 15:14:45 -0500

    Updated METIS target environment to include a clarifier that the target-environment will only produce internal effects.

--- integration/target-env/METIS/schema.ts
+++ integration/target-env/METIS/schema.ts
@@ -4,9 +4,10 @@ import TargetEnvSchema from '../../library/target-env-classes'
  * Represents the `METIS` target environment.
  */
 const METIS = new TargetEnvSchema({
-  name: 'METIS',
-  description: 'The METIS target environment.',
-  version: '0.2.0',
+  name: 'METIS (Internal Effects)',
+  description:
+    'A target-environment which allows effects to be produced within METIS itself, without interfacing with external software.',
+  version: '0.2.1',
 })
 
 export default METIS
