75ace71ad5b535433964d2bafc034752ec91aa9b
jacob-thomas12 <jacob.thomas@govcio.com>
2025-10-09 17:53:56 -0500

    Added BSON support to integration library and updated toolbox structure

---
+++ integration/library/toolbox/index.ts
@@ -0,0 +1,14 @@
+import * as BSON from 'bson'
+import BooleanToolbox from 'metis/toolbox/booleans'
+import NumberToolbox from 'metis/toolbox/numbers'
+import ObjectToolbox, { AnyObject } from 'metis/toolbox/objects'
+import VersionToolbox from 'metis/toolbox/versions'
+
+export {
+  AnyObject,
+  BooleanToolbox,
+  BSON,
+  NumberToolbox,
+  ObjectToolbox,
+  VersionToolbox,
+}
